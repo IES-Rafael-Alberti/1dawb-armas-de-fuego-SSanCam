83ea4887d554062072d59084c06fc62663729b44
Sara S Camilleri <ssancam391@g.educaand.es>
2024-02-16 15:35:01 +0100

    comienzo_parte_II

--- src/Main.kt
+++ src/Main.kt
@@ -20,40 +20,42 @@ fun main() {
     //CAJA DE MUNICION PARA RECARGA:
     val cajaMunicionExtra = cantidadMunicionExtra
 
-    // MAPA QUE REGISTRA ALEATORIAMENTE DE 1-3 DISPAROS POR CADA ARMA DE UN GRUPO DE 12
+    /*
+//RONDA DE TIROS HASTA ACABAR LA MUNICIÓN EXTRA.
+while (cantidadMunicionExtra > 0) {
+    for ((nombre, arma) in armero) {
+        println("**Quedan $cantidadMunicionExtra balas extras.**")
+        espacio()
+        if (arma.municion != 0) {
+            arma.disparar()
+            println(arma.toString())
+            espacio()
+            pausa()
+
+        } else {
+            println("$nombre ha agotado su munición y tiene que recargar.")
+            arma.recarga()
+            println(arma.toString())
+            espacio()
+            pausa()
+        }
+    }
+
+    if (cantidadMunicionExtra == 0) {
+        espacio()
+        println("Se han acabado las balas extras!!")
+        pausa()
+    }
+} */
+
+    //Deben dispararse 12 armas, cada una entre 1 y 3 veces... muestra también un mensaje que identifique cada comienzo de los 12 disparos
+
+    /*// MAPA QUE REGISTRA ALEATORIAMENTE DE 1-3 DISPAROS POR CADA ARMA DE UN GRUPO DE 12
     val disparos = (1..12).map { armero.random() to (1..3).random() }
     //DISPARAMOS Y RECARGAMOS LAS ARMAS, CUANDO SEA NECESARIO.
     println("Hay $cantidadMunicionExtra balas extras para todas las armas.")
     pausa()
     espacio()
-
-    /*
-    //RONDA DE TIROS HASTA ACABAR LA MUNICIÓN EXTRA.
-    while (cantidadMunicionExtra > 0) {
-        for ((nombre, arma) in armero) {
-            println("**Quedan $cantidadMunicionExtra balas extras.**")
-            espacio()
-            if (arma.municion != 0) {
-                arma.disparar()
-                println(arma.toString())
-                espacio()
-                pausa()
-
-            } else {
-                println("$nombre ha agotado su munición y tiene que recargar.")
-                arma.recarga()
-                println(arma.toString())
-                espacio()
-                pausa()
-            }
-        }
-
-        if (cantidadMunicionExtra == 0) {
-            espacio()
-            println("Se han acabado las balas extras!!")
-            pausa()
-        }
-    } */
 
     while (cantidadMunicionExtra > 0) {
         for ((index, disparo) in disparos.withIndex()) {
@@ -82,4 +84,8 @@ fun main() {
     espacio()
     println("Se acabaron las balas, nos vamos pa' casa.")
     pausa()
+    */
+
+    //1. Genera aleatoriamente disparos, en este caso 9, para las armas de fuego y para tres clases más (Casa, Coche y Bocadillo).
+
 }
